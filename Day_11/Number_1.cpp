962f8934b439edb36adc6fd4106f99fbc294c46d
Dibyajyoti-13 <dibyajyotisarkar05@gmail.com>
2024-09-13 10:31:17 +0530

    (^.^)

---
+++ Day_11/Number_1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void find(int a[],int target,int size){
+    for(int i=0;i<size;i++){
+        if (a[i]==target){
+            cout<<"True";
+            return;
+        }
+    }
+    cout<<"False";
+    return;
+}
+int main(){
+    int n,arr[100],target;
+    cout<<"Enter size: ";
+    cin>>n;
+    cout<<"Enter elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter Target: ";
+    cin>>target;
+    find(arr,target,n);
+}
