3a9f854d91d996f2643766091aabc006cb1745e1
Dibyajyoti-13 <dibyajyotisarkar05@gmail.com>
2024-09-27 23:28:22 +0530

    Done

--- Day_25/2.cpp
+++ Day_25/2.cpp
@@ -6,7 +6,7 @@ struct Node{
     int data;
     Node* next;
     Node(int n):data(n),next(nullptr){}
-}
+};
 
 void push(Node*& head,int data){
     Node* n=new Node(data);
@@ -18,7 +18,43 @@ void push(Node*& head,int data){
     head=n;
 }
 
+void pop(Node*& head){
+    if(head==nullptr){
+        cout<<"Empty Stack! Underflow.";
+        return;
+    }
+    cout<<head->data<<endl;
+    head=head->next;
+}
+void peek(Node* head){
+    if(head==nullptr){
+        cout<<"Empty Stack!";
+        return;
+    }
+    cout<<head->data<<endl;
+
+}
+
+void display(Node* head){
+    Node* current=head;
+    while(current!=nullptr){
+        cout<<current->data<<" ";
+        current=current->next;
+    }
+    cout<<endl;
+}
 
 int main(){
+    Node* head=nullptr;
+    int n;
+    do{
+        cout<<"Enter number or -1 to exit: ";
+        cin>>n;
+        if(n==-1) break;
+        push(head,n);
 
+    }while(n!=-1);
+    pop(head);
+    peek(head);
+    display(head);
 }
